3825d60e70c1fd74961ea10bbfe258ebb4325a26
s5421003 <114901226+s5421003@users.noreply.github.com>
2023-01-18 10:24:17 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
 function get_calc(btn) {
       if(btn.value == "=") {
-        document.dentaku.display.value = eval(document.dentaku.display.value);
+            document.dentaku.display.value = eval(document.dentaku.display.value);
       } else if (btn.value == "C") {
-        document.dentaku.display.value = "";
-      } else if(btn.value == "e") {
-        btn.value = Math.E;
-        document.dentaku.pi_btn.value = "e";
-      } else if(btn.value == "π") {
-        btn.value = Math.PI;
-        document.dentaku.pi_btn.value = "π";
-      } else if(btn.value == "sin") {
-        btn.value = Math.sin;
-        document.dentaku.pi_btn.value = "sin";
-      } else if(btn.value == "cos") {
-        btn.value = Math.cos;
-        document.dentaku.pi_btn.value = "cos";
-      } else if(btn.value == "tan") {
-        btn.value == Math.tan;
-        document.dentaku.pi_btn.value = "tan";
-      } else if(btn.value == "log") {
-        btn.value == Math.log;
-        document.dentaku.pi_btn.value = "log";
+            document.dentaku.display.value = "";
       } else {
         if (btn.value == "×") {
           btn.value = "*";
